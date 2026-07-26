1dbcb85d9b494c1938387921729bdb8b1e98678a
Kailey Liou <kaileyliou09@gmail.com>
2026-07-26 13:46:38 -0400

    finished week 1 code

---
+++ Week1.py
@@ -0,0 +1,35 @@
+name = "Kailey"
+hobby = "coding"
+
+age = 17
+score = 100
+
+print(5 + 3) # addition
+print(10 - 4) # subtraction
+print(6 * 2) # multiplication
+print(8 / 2) # division
+
+age = "13" # string
+age = 13 # integer
+
+print("5" + "5")
+print(5 + 5)
+
+print("Hello World")
+
+name = "Kailey"
+print("My name is", name)
+
+age = 17
+print("I am", age, "years old")
+
+x = 5
+y = 10
+print("The total is", x + y)
+
+name = "Kailey"
+age = 17
+favorite_subject = "math"
+print("My name is", name)
+print("I am", age, "years old")
+print("My favorite subject is", favorite_subject)
