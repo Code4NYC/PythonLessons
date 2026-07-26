3d9a227e2b2ac03613aecba959d45e1be6d5ea58
Kailey Liou <kaileyliou09@gmail.com>
2026-07-26 14:02:06 -0400

    finished week 4 code

---
+++ Week4.py
@@ -0,0 +1,43 @@
+for i in range(1, 16):
+    if i % 3 == 0:
+        print("Fizz")
+    else:
+        print(i)
+
+print("Hello!")
+len("Python")
+input("What's your name? ")
+
+def greet():
+    print("Hello!")
+
+greet()
+
+def add_numbers():
+    print(5 + 3)
+
+add_numbers()
+
+def print_name():
+    print("Kailey")
+
+print_name()
+
+fruit1 = "Apple"
+fruit2 = "Banana"
+fruit3 = "Orange"
+
+fruits = ["Apple", "Banana", "Orange"]
+
+print(fruits[0])
+
+foods = ["Pizza", "Tacos", "Pasta"]
+print(foods)
+print(foods[0])
+
+def print_movies(movie_list):
+    for movie in movie_list:
+        print(movie)
+
+movies = ["Cars", "Toy Story", "Finding Nemo"]
+print_movies(movies)
