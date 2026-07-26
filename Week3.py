bd1bfe96adc81b335fcec29537209309fd0495c4
Kailey Liou <kaileyliou09@gmail.com>
2026-07-26 13:57:33 -0400

    uploaded week 3 code

---
+++ Week3.py
@@ -0,0 +1,45 @@
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+print("Hello")
+print("Hello")
+print("Hello")
+
+for i in range(3):
+    print("Hello")
+
+for i in range(3):
+    print(i)
+
+for i in range(1, 6):
+    print(i)
+
+for i in range(3):
+    print("Kailey")
+
+count = 1
+while count <= 3:
+    print(count)
+    count += 1
+
+# while True:
+#     print("Hi")
+
+for i in range(1, 6):
+    if i % 2 == 0:
+        print(i)
+
+for i in range (1, 11):
+    if i > 5:
+        print(i)
+
+for i in range(2):
+    for j in range(2):
+        print(i, j)
+
+num = int(input("Enter a number: "))
+for i in range(1, num + 1):
+    print(i)
